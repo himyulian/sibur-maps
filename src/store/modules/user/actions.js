d2a33085d62a5737f74248f8381c20e4af623625
himyulian <armiya12@mail.ru>
2019-09-03 16:55:30 +0300

    refactor

--- src/store/modules/user/actions.js
+++ src/store/modules/user/actions.js
@@ -6,9 +6,6 @@ export async function fetchCurrentUser ({ commit, getters }) {
     let currentUser = await sp.profiles.myProperties.get()
     commit('setLoadingStatus', false)
     commit('setCurrentUser', currentUser)
-    console.log('currentUser', currentUser)
-    console.log('currentUserProps', getters.getCurrentUserProps)
-    console.log('currentUserCompany', getters.getCurrentUserCompany)
   } catch (e) {
     console.error('Ошибка в получении текущего пользователя', e)
   }
