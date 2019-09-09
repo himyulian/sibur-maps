aa7d1bc09f41d978d9071e0c4b235318e85be034
himyulian <armiya12@mail.ru>
2019-09-09 08:42:50 +0300

    add geojson layers to actFetchMapVSKmodePROD

--- src/store/modules/mapVSK/actions.js
+++ src/store/modules/mapVSK/actions.js
@@ -51,6 +51,13 @@ export async function actFetchMapVSKmodePROD ({ commit }) {
     let mainConstrunctions  = await axios.get('https://sharepoint/orgunits/vsk/map/SiteAssets/GeoJSON/VSK/EPSG4326/vsk_main_construnctions.geojson')
     let mainRailways        = await axios.get('https://sharepoint/orgunits/vsk/map/SiteAssets/GeoJSON/VSK/EPSG4326/vsk_main_railways.geojson')
     let mainRoads           = await axios.get('https://sharepoint/orgunits/vsk/map/SiteAssets/GeoJSON/VSK/EPSG4326/vsk_main_roads.geojson')
+    let d1Landuse           = await axios.get('https://sharepoint/orgunits/vsk/map/SiteAssets/GeoJSON/VSK/EPSG4326/vsk_D1_landuse.geojson')
+    let d1Construnctions    = await axios.get('https://sharepoint/orgunits/vsk/map/SiteAssets/GeoJSON/VSK/EPSG4326/vsk_D1_construnctions.geojson')
+    let d1Railways          = await axios.get('https://sharepoint/orgunits/vsk/map/SiteAssets/GeoJSON/VSK/EPSG4326/vsk_D1_railways.geojson')
+    let d1Roads             = await axios.get('https://sharepoint/orgunits/vsk/map/SiteAssets/GeoJSON/VSK/EPSG4326/vsk_D1_roads.geojson')
+    let o61Landuse          = await axios.get('https://sharepoint/orgunits/vsk/map/SiteAssets/GeoJSON/VSK/EPSG4326/vsk_61_landuse.geojson')
+    let o61Construnctions   = await axios.get('https://sharepoint/orgunits/vsk/map/SiteAssets/GeoJSON/VSK/EPSG4326/vsk_61_construnctions.geojson')
+    let o61Roads            = await axios.get('https://sharepoint/orgunits/vsk/map/SiteAssets/GeoJSON/VSK/EPSG4326/vsk_61_roads.geojson')
     commit('setLoadingStatus', false)
     let response = {
       main: {
@@ -59,7 +66,17 @@ export async function actFetchMapVSKmodePROD ({ commit }) {
         railways: mainRailways.data,
         roads: mainRoads.data
       },
-
+      d1: {
+        landuse: d1Landuse.data,
+        construnctions: d1Construnctions.data,
+        railways: d1Railways.data,
+        roads: d1Roads.data
+      },
+      o61: {
+        landuse: o61Landuse.data,
+        construnctions: o61Construnctions.data,
+        roads: o61Roads.data
+      },
     }
     commit('setMapVSK', response)
   } catch (e) {
